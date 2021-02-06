fd42ecbe1f8747a4b2ea12665bcb63b37233d301
Jast19 <lhalexis19@gmail.com>
2021-02-06 17:19:59 -0500

    fist test

---
+++ src/test/java/co/techandsolve/mudanza/service/ProcessServiceTes.java
@@ -0,0 +1,11 @@
+package co.techandsolve.mudanza.service;
+
+import org.junit.jupiter.api.Test;
+
+public class ProcessServiceTes {
+
+    @Test
+    void getListFileTest(){
+        System.out.println("Hello test");
+    }
+}
